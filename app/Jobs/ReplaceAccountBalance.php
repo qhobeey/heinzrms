efe917a40ddce812c1c8dfbfe3463b8183912f52
qhobeey <bludohric@gmail.com>
2019-02-02 01:38:59 +0000

    c

--- app/Jobs/ReplaceAccountBalance.php
+++ app/Jobs/ReplaceAccountBalance.php
@@ -34,7 +34,7 @@ class ReplaceAccountBalance implements ShouldQueue
       \App\Bill::orderBy('id')->where('year', 2018)->chunk(1000, function ($bills) {
         \App\Processing::create(['total' => \App\Bill::where('year', 2018)->count(), 'count' => 0, 'percentage' => 0]);
         foreach ($bills as $key => $bill):
-          $cc = floatval($bill->current_amount;)
+          $cc = floatval($bill->current_amount);
           $vv = floatval($bill->account_balance);
           $bill->current_amount = $vv;
           $bill->account_balance = $cc;
