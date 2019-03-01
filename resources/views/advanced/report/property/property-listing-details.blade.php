5795bc8a4f7523d2ccdf0f5608b9f1e17160f274
qhobeey <bludohric@gmail.com>
2019-03-01 17:52:17 +0000

    .

--- resources/views/advanced/report/property/property-listing-details.blade.php
+++ resources/views/advanced/report/property/property-listing-details.blade.php
@@ -59,7 +59,7 @@
                 <td><?= $key+1; ?></td>
                 <td><?= $bill->account_no; ?></td>
                 <td style="font-size: 11px;"><?= $bill->property ? ($bill->property->owner ? $bill->property->owner->name: 'NA'): 'NA'; ?></td>
-                <td style="font-size: 11px;"><?= $bill->property ? ($bill->property->owner ? ($bill->property->owner->address ?: 'NA'): 'NA'): 'NA'; ?></td>
+                <td style="font-size: 11px;"><?= $bill->property ? ($bill->property->address ?: 'NA'): 'NA'; ?></td>
                 <td style="font-size: 11px;"><?= $bill->property ? ($bill->property->category ? $bill->property->category->description: 'NA'): 'NA'; ?></td>
                 <td><?= \App\Repositories\ExpoFunction::formatMoney($bill->arrears, true); ?></td>
                 <td><?= \App\Repositories\ExpoFunction::formatMoney($bill->current_amount, true); ?></td>
