b91838a6c081d90f7a275fc2f5e6634fb67a78b5
qhobeey <bludohric@gmail.com>
2019-07-11 14:00:39 +0000

    .

--- resources/views/console/billing/property/bulk-print.blade.php
+++ resources/views/console/billing/property/bulk-print.blade.php
@@ -748,7 +748,7 @@ $(document).ready(function(){
         // return "GHc " + p[0].split("").reverse().reduce(function(acc, num, i, orig) {
         //     return  num=="-" ? acc : num + (i && !(i % 3) ? "," : "") + acc;
         // }, "") + "." + p[1];
-    }
+    // }
 
     function reloadPage() {
       document.getElementById('loadingCarf').style.display = "none"
--- resources/views/console/billing/property/print-bills.blade.php
+++ resources/views/console/billing/property/print-bills.blade.php
@@ -546,7 +546,7 @@ $(document).ready(function(){
         // return "GHc " + p[0].split("").reverse().reduce(function(acc, num, i, orig) {
         //     return  num=="-" ? acc : num + (i && !(i % 3) ? "," : "") + acc;
         // }, "") + "." + p[1];
-    }
+    // }
 
     function reloadPage() {
       document.getElementById('loadingCarf').style.display = "none"
